44589981211be03f4c71583ac5186e82575d644a
François NOUAILLE-DEGORCE <you@example.com>
2021-08-13 00:01:13 +0200

    wip

--- patch.py
+++ patch.py
@@ -6,8 +6,8 @@ pix = im.load()
 print(im.size)  # Get the width and hight of the image for iterating over
 size = min(im.size[0], im.size[1])
 rect = Image.new("RGBA", (size, size), (255, 255, 255, 0))
-im.paste(rect, (int(im.size[0]/2-size/2),int(im.size[1]/2-size/2)))
+rect.paste(im,  (int(-(im.size[0] / 2 - size / 2)),(int(-(im.size[1] / 2 - size / 2)))))
 draw = ImageDraw.Draw(rect)
-draw.ellipse((im.size[0]/2-size/2, im.size[1]/2-size/2, im.size[0]/2+size/2, im.size[1]/2+size/2),width=10, outline=(0, 0, 0))
-draw.ellipse((im.size[0]/2-size/2+25, im.size[1]/2-size/2+25, im.size[0]/2+size/2-25, im.size[1]/2+size/2-25),width=10, outline=(0, 0, 0))
+draw.ellipse(((0,0),( rect.size[0], rect.size[0])),width=10, outline=(0, 0, 0))
+draw.ellipse(((25,25), (rect.size[0]-25, rect.size[0]-25)),width=10, outline=(0, 0, 0))
 rect.save(sys.argv[2])  # Save the modified pixels as .png
