ba95f447ae837ffa1c1d60a56cd4351954a88543
François NOUAILLE-DEGORCE <you@example.com>
2021-08-13 01:04:38 +0200

    wip

--- logo2.bash
+++ logo2.bash
@@ -1,6 +1,8 @@
 #!/bin/sh
 python3 squarify.py wolf.png tmp.png
-python3 patch.py tmp.png
-python3 black_white.py tmp.png
+
 
 python3 merge.py tmp.png cross.jpg tmp.png
+
+python3 black_white.py tmp.png
+python3 patch.py tmp.png
