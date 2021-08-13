79fccf543740ae53bcea847fcf10695f9956c2f7
François NOUAILLE-DEGORCE <you@example.com>
2021-08-13 03:12:58 +0200

    wip

--- draw_text_on_circle.py
+++ draw_text_on_circle.py
@@ -19,16 +19,16 @@ def draw_char(s, module, arg):
     pos = Vector2D(0, module)
     pos = pos.setArg(arg * 0.0174533)
     pos = pos + center
-    txt=Image.new('L', (50,int(sys.argv[3])))
+    txt=Image.new('L', (int(sys.argv[3]),int(sys.argv[3])))
     d = ImageDraw.Draw(txt)
     d.text( (0, 0), s,  font=f, fill=255)
     w=txt.rotate(-arg - 90,  expand=1)
-    im.paste( ImageOps.colorize(w, (0,0,0), (255,0,0)), (int(pos.x),int(pos.y)),  w)
+    im.paste( ImageOps.colorize(w, (0,0,0), (255,0,0)), (int(pos.x-w.size[0]/2),int(pos.y-w.size[1]/2)),  w)
 def draw_text(s, module, arg):
     offset = 0
 
     for c in s:
         draw_char(c, module, arg - offset)
-        offset = offset + 10
-draw_text( "Salut c'est moi! trololo !!! 1 2 3 4 5 ", 500, 180)
+        offset = offset + 6
+draw_text( "Salut c'est moi!", 500, 180)
 im.save(sys.argv[1])  # Save the modified pixels as .png
