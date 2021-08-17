78ca5306403b2debc30bbb11df836677861ddfd1
François NOUAILLE-DEGORCE <you@example.com>
2021-08-17 11:58:07 +0200

    WIP

--- logo3.bash
+++ logo3.bash
@@ -3,5 +3,6 @@ cp wolf.png tmp.png
 ./squarify.py tmp.png
 
 ./draw_circle.py tmp.png
+./draw_text_on_circle.py tmp.png "Libertas·potior·quam·saturitas" 500 /mnt/c/Windows/Fonts/arial.ttf 50
+./black_white.py tmp.png
 ./keep_only_the_circle.py tmp.png
-./draw_text_on_circle.py tmp.png "Libetas potiorquam saturitas" 500 /mnt/c/Windows/Fonts/arial.ttf 50
