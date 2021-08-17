b022366427483a10ac9d5a0cc504b4a8c1b6a400
François NOUAILLE-DEGORCE <you@example.com>
2021-08-17 12:03:50 +0200

    wip: lot of files with small difference to make it easier?

--- logo3.bash
+++ logo3.bash
@@ -3,6 +3,7 @@ cp wolf.png tmp.png
 ./squarify.py tmp.png
 
 ./draw_circle.py tmp.png
-./draw_text_on_circle.py tmp.png "Libertas·potior·quam·saturitas" 500 /mnt/c/Windows/Fonts/arial.ttf 50
+./draw_text_on_circle_up.py tmp.png "Libertas·potior" 500 /mnt/c/Windows/Fonts/arial.ttf 50
+./draw_text_on_circle_down.py tmp.png "quam saturitas" 500 /mnt/c/Windows/Fonts/arial.ttf 50
 ./black_white.py tmp.png
 ./keep_only_the_circle.py tmp.png
