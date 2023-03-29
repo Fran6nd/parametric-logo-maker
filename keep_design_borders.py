ae0a7789932c6a38cb3dc133391420e070b22f2a
Francois NOUAILLE-DEGORCE <my@secret.email>
2023-03-29 23:50:08 +0200

    wip

--- keep_design_borders.py
+++ keep_design_borders.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import sys
-from PIL import Image
+from PIL import Image, ImageDraw
+from vector2D import Vector2D
 
 
 im = Image.open(sys.argv[1])
@@ -26,10 +27,12 @@ def copy(array):
     return output
     
 basepix = copy(pix)
+points = list()
+i = 0
 for x in range(0,im.size[0]):
     for y in range(0,im.size[1]):
         col = basepix[x][y]
-        if col[0] < 200:
+        if col[0] < 128:
             poss = build_possibilities(x,y,im.size)
             blank = 0
             for p in poss:
@@ -38,8 +41,25 @@ for x in range(0,im.size[0]):
             if blank:
                 pix[x, y]= (0,0,0,255)
             else:
+                i += 1
+                if i == 50:
+                    i = 0
+                    points.append([Vector2D(x,y), 0])
                 pix[x, y]= (255,255,255,255)
-        else:
-            pix[x, y]= (255,255,255,255)
+draw = ImageDraw.Draw(im)
+
+i = 10
+for n in range(len(points)):
+    i -= 1
+    if points[n][1] == 0 and i == 0:
+        points[n][1] = 1
+        i = n + 10
+        for m in range(len(points)):
+            dist = (points[m][0] - points[n][0]).getModule()
+            if dist > 50 and points[m][1] == 0:
+                
+                points[m][1] = 1
+                draw.line((points[m][0].x, points[m][0].y,points[n][0].x,points[n][0].y), 255)
+                break
 
 im.save(sys.argv[1])
--- logo2.bash
+++ logo2.bash
@@ -2,10 +2,13 @@
 cp wolf.png tmp.png
 ./squarify.py tmp.png
 
-./draw_circle.py tmp.png
-./merge.py tmp.png cross.jpg
+#./draw_circle.py tmp.png
+#./merge.py tmp.png cross.jpg
 
 
-./draw_circle.py tmp.png
-./keep_only_the_circle.py tmp.png
-./black_white.py tmp.png
+#./draw_circle.py tmp.png
+#./keep_only_the_circle.py tmp.png
+
+
+./keep_borders.py tmp.png
+#./black_white.py tmp.png
